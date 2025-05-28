4893eade14018f1c69535bbfaaf7c500f6447a0b
M1keEm <michalbanaszek03@gmail.com>
2025-05-28 13:30:44 +0200

    backend Dockerfile db connection fix

--- backendFlask/config.py
+++ backendFlask/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     DB_USER = os.getenv("USERNAME")
     DB_PASS = os.getenv("PASSWORD")
-    DB_HOST = "localhost"  # Use service name for Docker
+    DB_HOST = "db"  # Use service name for Docker
     DB_PORT = os.getenv("DATABASE_PORT")
     DB_NAME = os.getenv("DATABASE_NAME", "").replace('"', '')
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
